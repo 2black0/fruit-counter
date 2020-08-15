02cbc73b427c21589607dde67b55dac657791380
2black0 <2black0@gmail.com>
2020-08-15 23:09:50 +0700

    add testing code

--- software/testing/color/color.ino
+++ software/testing/color/color.ino
@@ -1,8 +1,3 @@
-/*
-tutorial kalibrasi:
-https://randomnerdtutorials.com/arduino-color-sensor-tcs230-tcs3200/
-*/
-
 const int s0Pin = 4;
 const int s1Pin = 5;
 const int s2Pin = 6;
@@ -13,8 +8,12 @@ int redFrequency = 0;
 int greenFrequency = 0;
 int blueFrequency = 0;
 
-void setup() {
+void init_device() {
   Serial.begin(9600);
+  delay(50);
+}
+
+void init_pin() {
   pinMode(s0Pin, OUTPUT);
   pinMode(s1Pin, OUTPUT);
   pinMode(s2Pin, OUTPUT);
@@ -24,25 +23,32 @@ void setup() {
   digitalWrite(s0Pin, HIGH);
   digitalWrite(s1Pin, LOW);
 }
+
+void setup() {
+  init_device();
+  init_pin();
+  Serial.println("Color Sensor Testing");
+}
+
 void loop() {
   digitalWrite(s2Pin, LOW);
   digitalWrite(s3Pin, LOW);
   redFrequency = pulseIn(soutPin, LOW);
-  Serial.print("R = ");
+  Serial.print("R= ");
   Serial.print(redFrequency);
   delay(100);
 
   digitalWrite(s2Pin, HIGH);
   digitalWrite(s3Pin, HIGH);
   greenFrequency = pulseIn(soutPin, LOW);
-  Serial.print(" G = ");
+  Serial.print(" G= ");
   Serial.print(greenFrequency);
   delay(100);
 
   digitalWrite(s2Pin, LOW);
   digitalWrite(s3Pin, HIGH);
   blueFrequency = pulseIn(soutPin, LOW);
-  Serial.print(" B = ");
+  Serial.print(" B= ");
   Serial.println(blueFrequency);
   delay(100);
 }
